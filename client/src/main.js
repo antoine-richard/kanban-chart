0b938923e3de1c925f834ef3e96b2604bf995584
Antoine Richard <richard.antoine@gmail.com>
2014-10-14 09:50:57 +0200

    renamed ctrl. added directive requiring d3 (test)

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
 require('angular/angular');
 require('angular-route/angular-route');
 
+// tmp
+require('./cfd/directives/diagram');
+
 angular.module('app', ['ngRoute'])
 
-.controller('CfdCtrl', require('./cfd/controllers/cfd'))
+.controller('CfdCtrl', require('./cfd/controllers/input'))
 .factory(   'work',    require('./cfd/services/work'))
 
 .config(function($routeProvider) {
